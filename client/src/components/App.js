923ea2e935ab3ba261f711e6d4e2caf9df0a8aff
Laura Gonzalez <lg458@cornell.edu>
2022-11-01 13:36:48 -0700

    initial components set up

---
+++ client/src/components/App.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import NavBar from './NavBar'
+import LandingPage from './LandingPage'
+import Home from './Home'
+import Login from './Login'
+import SignUp from './SignUp'
+import Favorites from './Favorites'
+import ShoppingList from './ShoppingList'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<LandingPage />}/>
+          <Route path="/home" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/signup" element={<SignUp />}/>
+          <Route path="/favorites" element={<Favorites />}/>
+          <Route path="/shoppinglist" element={<ShoppingList />}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
