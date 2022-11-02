32ecb1ff0885a7b4a8c431a00e182eb014da874f
Kevin Gicheha <gichehakevin@gmail.com>
2022-11-02 15:49:25 -0700

    added login/signup pages

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,121 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { styled, Stack } from "@mui/material";
+import { red } from "@mui/material/colors";
+import { useForm } from "react-hook-form";
+import {
+  Typography,
+  Button,
+  CssBaseline,
+  Container,
+  InputLabel,
+  Input,
+  TextField,
+  Box,
+} from "@mui/material";
 
 function Login() {
+  //storing data from the form
+  const { register, handleSubmit } = useForm();
+
+  //to display the errors
+  const [errors, setErrors] = useState([]);
+
+  //gives you access to the history instance that you may use to navigate.
+  // const history = useHistory();
+
+  const onSubmit = (data) => {
+    // e.preventDefault();
+    // make post request on submit
+    // fetch(`/login`, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Accept: "application/json",
+    //   },
+    //   body: JSON.stringify(data),
+    // }).then((res) => {
+    //   if (res.ok) {
+    //     res.json().then((user) => {
+    //       updateUser(user);
+    //       history.push(`/home/${user.id}`);
+    //     });
+    //   } else {
+    //     res.json().then((json) => setErrors(json.errors));
+    //   }
+    // });
+  };
+
+  // CUSTOM CSS
+  const CustomButton = styled(Button)({
+    fontWeight: "bold",
+    backgroundColor: "#078080",
+    color: "#ffffff",
+    // color: "#232323",
+    "&:hover": {
+      backgroundColor: "#137474",
+    },
+  });
+  // CUSTOM CSS ^^
+
   return (
-    <div>Login</div>
-  )
+    <>
+      <CssBaseline />
+      <div id="loginDiv">
+        <Stack spacing={2}>
+          {/* <Container maxWidth="max-content"> */}
+            <img src="/signInPagePhoto.jpg" alt="saladPlate" height="400" width="400"/>
+          {/* </Container> */}
+
+          <div className="customForm">
+            {/* <Container> */}
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <Typography variant="h6" id ="signInHeader">Sign in</Typography>
+                {errors ? (
+                  <div className="displayedErrors">{errors}</div>
+                ) : null}
+                <br />
+                <Box mb={2}>
+                  <TextField
+                    type="email"
+                    label="email"
+                    fullWidth
+                    variant="outlined"
+                    {...register("email")}
+                  />
+                </Box>
+                <Box mb={2}>
+                  <TextField
+                    type="password"
+                    label="password"
+                    variant="outlined"
+                    fullWidth
+                    {...register("password")}
+                  />
+                </Box>
+                <CustomButton
+                  type="submit"
+                  size="large"
+                  variant="contained"
+                  fullWidth
+                >
+                  Sign in
+                </CustomButton>
+              </form>
+              <br />
+              <Typography variant="subtitle1">
+                New User?
+                <a id="signuplink" href="/signup">
+                  {" "}
+                  Create an account
+                </a>
+              </Typography>
+            {/* </Container> */}
+          </div>
+        </Stack>
+      </div>
+    </>
+  );
 }
 
-export default Login
+export default Login;
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,9 +1,164 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material";
+import { red } from "@mui/material/colors";
+import {
+  Typography,
+  Button,
+  CssBaseline,
+  Container,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Box,
+  TextField,
+} from "@mui/material";
+import { useForm } from "react-hook-form";
 
 function SignUp() {
+  //storing data from the form
+  const { register, handleSubmit } = useForm();
+  //to display the errors
+  const [errors, setErrors] = useState([]);
+
+  //gives you access to the history instance that you may use to navigate.
+  //  const history = useHistory();
+
+  const onSubmit = (data) => {
+    // e.preventDefault();
+    console.log(data);
+
+    // make post request on submit
+    // fetch(`/signup`, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Accept: "application/json",
+    //   },
+    //   body: JSON.stringify(data),
+    // }).then((res) => {
+    //   if (res.ok) {
+    //     res.json().then((user) => {
+    //       updateUser(user);
+    //       history.push(`/login`);
+    //     });
+    //   } else {
+    //     res.json().then((json) => setErrors(Object.entries(json.errors)));
+    //   }
+    // });
+  };
+
+  // CUSTOM CSS
+  const CustomButton = styled(Button)({
+    fontWeight: "bold",
+    backgroundColor: "#078080",
+    color: "#ffffff",
+    // color: "#232323",
+    "&:hover": {
+      backgroundColor: "#137474",
+    },
+  });
+  // CUSTOM CSS ^^
+
   return (
-    <div>SignUp</div>
-  )
+    <>
+      <CssBaseline />
+      <div id="signUpDiv">
+        <Stack spacing={2}>
+          <Container maxWidth="max-content"></Container>
+          <div className="customForm">
+            <Container>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <br />
+                <Typography variant="h6">Create profile</Typography>
+                {errors
+                  ? errors.map((e) => (
+                      <div className="displayedErrors">
+                        {e[0] + ": " + e[1]}
+                      </div>
+                    ))
+                  : null}
+                <br />
+                <Box mb={2}>
+                  <TextField
+                    type="text"
+                    label="First Name*"
+                    id="outlined-error-helper-text"
+                    fullWidth
+                    variant="outlined"
+                    {...register("first_name")}
+                  />
+                </Box>
+                <Box mb={2}>
+                  <TextField
+                    type="text"
+                    label="Last Name*"
+                    id="outlined-error-helper-text"
+                    fullWidth
+                    variant="outlined"
+                    {...register("last_name")}
+                  />
+                </Box>
+
+                <Box mb={2}>
+                  <TextField
+                    type="email"
+                    label="email"
+                    id="outlined-error-helper-text"
+                    fullWidth
+                    variant="outlined"
+                    {...register("email")}
+                    placeholder="example@gmail.com"
+                  />
+                </Box>
+                <Box mb={2}>
+                  <TextField
+                    type="password"
+                    label="password"
+                    id="outlined-error-helper-text"
+                    fullWidth
+                    variant="outlined"
+                    {...register("password")}
+                    placeholder="*********"
+                  />
+                </Box>
+                <Box mb={2}>
+                  <TextField
+                    type="text"
+                    label="Allergies"
+                    id="outlined-error-helper-text"
+                    fullWidth
+                    variant="outlined"
+                    {...register("allergies")}
+                    placeholder="Ingredients you're allergies to"
+                  />
+                </Box>
+                <Box mb={2}>
+                  <CustomButton
+                    type="submit"
+                    size="large"
+                    variant="contained"
+                    fullWidth
+                  >
+                    Create Account
+                  </CustomButton>
+                </Box>
+                <Typography variant="subtitle1">
+                  Already have an an account?
+                  <a id="signuplink" href="/login">
+                    {" "}
+                    Sign in
+                  </a>
+                </Typography>
+              </form>
+            </Container>
+          </div>
+        </Stack>
+      </div>
+    </>
+  );
 }
 
-export default SignUp
+export default SignUp;
